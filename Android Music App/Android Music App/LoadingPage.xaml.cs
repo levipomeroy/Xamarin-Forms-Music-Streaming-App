b43206e3496c229d2a7e949d4af59f18a542cd85
levi pomeroy <levipomeroy@gmail.com>
2020-06-15 22:21:15 -0700

    Basic loading screen

--- Android Music App/Android Music App/LoadingPage.xaml.cs
+++ Android Music App/Android Music App/LoadingPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Android_Music_App.Models;
 using Android_Music_App.Services;
+using AngleSharp.Dom;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,23 +36,40 @@ namespace Android_Music_App
 
         public async void LoadPlaylist(PlaylistObject selectedPlaylist)
         {
+            AddNewLoadingStatus("Getting playlist info");
+
             //Store that this playlist was selected
             FileManager.AddPlaylist(selectedPlaylist);
 
             //Get songs in playlist
+            PreviousStepComplete();
+            AddNewLoadingStatus("Gathering songs in playlist");
+
             var songsInPlayListClient = new PlaylistItemsSearch();
             var songs = await songsInPlayListClient.GetPlaylistItems(selectedPlaylist.Url);
+            PreviousStepComplete();
+            AddNewLoadingStatus("Shuffling");
             songs.Shuffle();
+
             var chosenPlaylist = new List<SearchResultsObject>(songs.Select(x => new SearchResultsObject(x.getTitle(), x.getThumbnail(), GetSongIdFromUrl(x.getUrl()))));
 
             //Download first song in playlist
+            PreviousStepComplete();
+            AddNewLoadingStatus("Downloading");
+
             var firstSong = chosenPlaylist.FirstOrDefault();
             await FileManager.DownloadSingleSong(firstSong);
+
+            PreviousStepComplete();
+            AddNewLoadingStatus("Retrieving album art");
 
             var trackInfo = Itunes.GetDataFromItunes(firstSong, 1);
             chosenPlaylist.FirstOrDefault().Title = trackInfo.Title;
             chosenPlaylist.FirstOrDefault().ImageSource = trackInfo.ImageSource;
             chosenPlaylist.FirstOrDefault().Artist = trackInfo.Artist;
+
+            PreviousStepComplete();
+            AddNewLoadingStatus("Playing");
 
             //load media player page 
             await Navigation.PushModalAsync(new NavigationPage(new MediaPlayerPage(chosenPlaylist)));
@@ -59,8 +77,13 @@ namespace Android_Music_App
 
         public async void LoadRecentlyPlayedPlaylists()
         {
+            AddNewLoadingStatus("Getting playlist info");
+
             var recentlyPlayedPlaylistSongs = new List<SearchResultsObject>();
             var songsInPlayListClient = new PlaylistItemsSearch();
+
+            PreviousStepComplete();
+            AddNewLoadingStatus("Gathering songs in playlist");
 
             foreach (var playlist in FileManager.GetPlaylists().Take(4))
             {
@@ -68,34 +91,53 @@ namespace Android_Music_App
                 recentlyPlayedPlaylistSongs.AddRange(songs.Select(x => new SearchResultsObject(x.getTitle(), x.getThumbnail(), GetSongIdFromUrl(x.getUrl()))));
             }
 
+            PreviousStepComplete();
+            AddNewLoadingStatus("Shuffling");
             recentlyPlayedPlaylistSongs.Shuffle();
 
             //Download first song in playlist
+            PreviousStepComplete();
+            AddNewLoadingStatus("Downloading");
             var firstSong = recentlyPlayedPlaylistSongs.FirstOrDefault();
             await FileManager.DownloadSingleSong(firstSong);
 
+            PreviousStepComplete();
+            AddNewLoadingStatus("Retrieving album art");
             var trackInfo = Itunes.GetDataFromItunes(firstSong, 1);
             recentlyPlayedPlaylistSongs.FirstOrDefault().Title = trackInfo.Title;
             recentlyPlayedPlaylistSongs.FirstOrDefault().ImageSource = trackInfo.ImageSource;
             recentlyPlayedPlaylistSongs.FirstOrDefault().Artist = trackInfo.Artist;
 
+            PreviousStepComplete();
+            AddNewLoadingStatus("Playing");
             await Navigation.PushModalAsync(new NavigationPage(new MediaPlayerPage(new List<SearchResultsObject>(recentlyPlayedPlaylistSongs))));
         }
 
         public async void LoadSavedSongs()
         {
+            AddNewLoadingStatus("Getting playlist info");
+
+            PreviousStepComplete();
+            AddNewLoadingStatus("Gathering songs in playlist");
             var savedSongs = FileManager.GetSavedSongs();
+
+            PreviousStepComplete();
+            AddNewLoadingStatus("Shuffling");
             savedSongs.Shuffle();
 
             //Download first song in playlist
             var firstSong = savedSongs.FirstOrDefault();
             await FileManager.DownloadSingleSong(firstSong);
 
+            PreviousStepComplete();
+            AddNewLoadingStatus("Retrieving album art");
             var trackInfo = Itunes.GetDataFromItunes(firstSong, 1);
             savedSongs.FirstOrDefault().Title = trackInfo.Title;
             savedSongs.FirstOrDefault().ImageSource = trackInfo.ImageSource;
             savedSongs.FirstOrDefault().Artist = trackInfo.Artist;
 
+            PreviousStepComplete();
+            AddNewLoadingStatus("Playing");
             await Navigation.PushModalAsync(new NavigationPage(new MediaPlayerPage(new List<SearchResultsObject>(savedSongs))));
         }
 
@@ -104,6 +146,22 @@ namespace Android_Music_App
         {
             var query = HttpUtility.ParseQueryString(new Uri(url).Query);
             return query["v"];
+        }
+
+        public void AddNewLoadingStatus(string status)
+        {
+            mainLoadingLayout.Children.Add(new Label
+            {
+                Text = status,
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = 20,
+                Style = Application.Current.Resources["MaterialIcons"] as Style
+            });
+        }
+
+        public void PreviousStepComplete()
+        {
+            (mainLoadingLayout.Children.LastOrDefault() as Label).Text += "\t\U000f05e0";
         }
 
     }
